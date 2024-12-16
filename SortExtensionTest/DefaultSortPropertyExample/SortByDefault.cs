d660308ebb2a0f791017fd86bf2ae685cbead9ed
elham liri <e.liri@ymail.com>
2024-12-16 12:14:58 +0330

    #DefaultSortPropertySampleMockData

---
+++ SortExtensionTest/DefaultSortPropertyExample/SortByDefault.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SortHelper;
+using SortHelper.Enums;
+
+namespace SortExtensionTest.DefaultSortPropertyExample
+{
+    public class SortByDefault
+    {
+        public List<TaskModel> SortTasksByDefault()
+        {
+            var tasks = GetCollection();
+            return tasks.ToList(); //.OrderBy("", SortDirection.Ascending).ToList();
+        }
+
+        private IQueryable<TaskModel> GetCollection()
+        {
+            var path = @$"{FilesPath()}\MOCK_DATA_Task.json";
+
+            var data = File.ReadAllText(path);
+            var dataList = JsonConvert.DeserializeObject<List<TaskModel>>(data) ?? new List<TaskModel>();
+            return dataList.AsQueryable();
+        }
+
+        private string FilesPath()
+        {
+            var appPath = Directory.GetCurrentDirectory();
+            appPath = appPath.Remove(appPath.IndexOf("bin"));
+
+            return $@"{appPath}\Files\";
+        }
+    }
+}
--- SortExtensionTest/Program.cs
+++ SortExtensionTest/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Sample project");
+using SortExtensionTest.DefaultSortPropertyExample;
+
+Console.WriteLine("Test Sort By Default");
+
+var defaultSorter = new SortByDefault();
+var sortedList = defaultSorter.SortTasksByDefault();
+
+foreach (var taskModel in sortedList)
+{
+    Console.WriteLine($"{taskModel.Title} Created on {taskModel.CreateDate.ToShortDateString()}");
+}
 
