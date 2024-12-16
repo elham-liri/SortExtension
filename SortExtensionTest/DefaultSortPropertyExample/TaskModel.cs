4113e8d8d949dfcbd9b3f96f93876d459c6c1210
elham liri <e.liri@ymail.com>
2024-12-16 11:28:20 +0330

    #DefaultSortPropertySample

---
+++ SortExtensionTest/DefaultSortPropertyExample/TaskModel.cs
@@ -0,0 +1,18 @@
+﻿using SortHelper.Attributes;
+
+namespace SortExtensionTest.DefaultSortPropertyExample
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [DefaultSortProperty]
+        public DateTime CreateDate { get; set; }
+
+        public int AssignedToUserId { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+    }
+}
